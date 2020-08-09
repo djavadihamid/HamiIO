0cba1dd0a2acb1a05bc08b1fbbbf39a6ac74ad23
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-08-08 22:49:10 -0700

    [INIT]

---
+++ Assets/Scripts/HamiIO/CONSTS.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace HamiModuleXmlIO
+{
+    public class CONSTS
+    {
+        public static readonly string __PROJECT_PATH_IN_RESOURCES =
+            $"{Application.dataPath}\\Resources\\Modules\\{ProjectName()}\\";
+
+        public static string ProjectName()
+        {
+            string[] s = Application.dataPath.Split('/');
+            return s[s.Length - 2];
+        }
+    }
+}
---
+++ Assets/Scripts/HamiIO/HamiXmlIO.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace HamiModuleXmlIO
+{
+    public class HamiXmlIO
+    {
+        public static T Read<T>(string fileName) where T : class
+        {
+            if (!File.Exists($"{CONSTS.__PROJECT_PATH_IN_RESOURCES}{fileName}.xml"))
+                return null;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            return xmlSerializer.Deserialize(
+                new StringReader(File.ReadAllText($"{CONSTS.__PROJECT_PATH_IN_RESOURCES}{fileName}.xml"))) as T;
+        }
+
+        public static void Write<T>(string fileName, T toSerialize)
+        {
+            if (!Directory.Exists(CONSTS.__PROJECT_PATH_IN_RESOURCES))
+                Directory.CreateDirectory(CONSTS.__PROJECT_PATH_IN_RESOURCES);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            var write = new StreamWriter($"{CONSTS.__PROJECT_PATH_IN_RESOURCES}{fileName}.xml");
+            xmlSerializer.Serialize(write.BaseStream, toSerialize);
+            write.Close();
+        }
+        
+    }
+}
