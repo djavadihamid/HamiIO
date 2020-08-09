028122188d64490ed7f2aa2918d8f1f9cf688f69
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-08-09 01:21:36 -0700

    [BUGFIX] delete extra character in path

--- Assets/Scripts/HamiIO/HamiModuleXmlIO.cs
+++ Assets/Scripts/HamiIO/HamiModuleXmlIO.cs
@@ -8,28 +8,28 @@ namespace HamiIO
     {
         public static T Read<T>(string folderName, string fileName) where T : class
         {
-            if (!File.Exists($"{CONSTS.__FULL_PATH_TO_RESOURCES}\\{folderName}\\{fileName}.xml"))
+            if (!File.Exists($"{CONSTS.__FULL_PATH_TO_RESOURCES}{folderName}\\{fileName}.xml"))
                 return null;
 
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
             return xmlSerializer.Deserialize(
                     new StringReader(
-                        File.ReadAllText($"{CONSTS.__FULL_PATH_TO_RESOURCES}\\{folderName}\\{fileName}.xml")))
+                        File.ReadAllText($"{CONSTS.__FULL_PATH_TO_RESOURCES}{folderName}\\{fileName}.xml")))
                 as T;
         }
 
         public static void Write<T>(string folderName, string fileName, T toSerialize)
         {
-            if (!Directory.Exists(CONSTS.__FULL_PATH_TO_RESOURCES))
-                Directory.CreateDirectory(CONSTS.__FULL_PATH_TO_RESOURCES);
+            if (!Directory.Exists(CONSTS.__FULL_PATH_TO_RESOURCES + folderName))
+                Directory.CreateDirectory(CONSTS.__FULL_PATH_TO_RESOURCES + folderName);
 
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
-            var write = new StreamWriter($"{CONSTS.__FULL_PATH_TO_RESOURCES}\\{folderName}\\{fileName}.xml");
+            var write = new StreamWriter($"{CONSTS.__FULL_PATH_TO_RESOURCES}{folderName}\\{fileName}.xml");
             xmlSerializer.Serialize(write.BaseStream, toSerialize);
             write.Close();
 
             MonoBehaviour.print(
-                $"Xml successfully has been written in {CONSTS.__FULL_PATH_TO_RESOURCES}\\{folderName}\\{fileName}.xml");
+                $"Xml successfully has been written in {CONSTS.__FULL_PATH_TO_RESOURCES}{folderName}\\{fileName}.xml");
         }
     }
 }
