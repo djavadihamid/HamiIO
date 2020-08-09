87e556ba358114c106b48d0879d6e4f5aa874ee8
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-08-09 04:39:39 -0700

    [SMALLCHANGE] some little refactoring

--- Assets/Scripts/HamiIO/CONSTS.cs
+++ Assets/Scripts/HamiIO/CONSTS.cs
@@ -4,9 +4,9 @@ namespace HamiIO
 {
     public class CONSTS
     {
-        public static readonly string __PATH_WITHIN_RESOURCES = $"Modules\\";
-
         public static readonly string __FULL_PATH_TO_RESOURCES =
             $"{Application.dataPath}\\Resources\\{__PATH_WITHIN_RESOURCES}";
+
+        public static readonly string __PATH_WITHIN_RESOURCES = $"Modules\\";
     }
 }
