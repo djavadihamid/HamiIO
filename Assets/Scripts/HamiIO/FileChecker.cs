bda72632723c8c1d4e9c96a7981bfc7f69a2e035
blackallballo <black.allballo@yandex.com>
2020-08-12 13:23:27 +0430

    [FEAT][JSON] json generatio has been added

---
+++ Assets/Scripts/HamiIO/FileChecker.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace HamiIO
+{
+    internal class FileChecker
+    {
+        public static void CheckJson(string folderPath, string fileName)
+        {
+            Check(folderPath, fileName, "json");
+        }
+
+        public static void CheckXml(string folderPath, string fileName)
+        {
+            Check(folderPath, fileName, "xml");
+        }
+
+        public static void CheckCs(string folderPath, string fileName)
+        {
+            Check(folderPath, fileName, "cs");
+        }
+
+        private static void Check(string folderPath, string fileName, string fileExtension)
+        {
+            string basePath     = CONSTS.__FULL_PATH_TO_RESOURCES + folderPath;
+            string completePath = $"{basePath}\\{fileName}.{fileExtension}";
+            if (!Directory.Exists(basePath)) Directory.CreateDirectory(basePath);
+            if (!File.Exists(completePath)) File.Create(completePath).Close();
+        }
+    }
+}
---
+++ Assets/Scripts/HamiIO/HamiModuleJsonConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace HamiIO
+{
+    public class HamiModuleJsonConfig
+    {
+        public static void Write<T>(string folderPath, string fileName, T obj)
+        {
+            FileChecker.CheckJson(folderPath, fileName);
+            File.WriteAllText($@"{CONSTS.__FULL_PATH_TO_RESOURCES}{folderPath}\{fileName}.json",
+                              JsonConvert.SerializeObject(obj, Formatting.Indented)
+                             );
+            MonoBehaviour.print($@"Json has been created in {CONSTS.__FULL_PATH_TO_RESOURCES}{folderPath}\{fileName}");
+        }
+    }
+}
