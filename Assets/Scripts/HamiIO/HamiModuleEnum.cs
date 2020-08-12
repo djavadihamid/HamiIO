df700d91602a7a2f15f5c55ec945cd90aaa6e06a
blackallballo <black.allballo@yandex.com>
2020-08-12 12:57:59 +0430

    [FEAT][ENUM] add Has() method to enum

--- Assets/Scripts/HamiIO/HamiModuleEnum.cs
+++ Assets/Scripts/HamiIO/HamiModuleEnum.cs
@@ -7,13 +7,18 @@ namespace HamiIO
     {
         public static void Write(string folderPath, string fileName, string[] items)
         {
-            string basePath = CONSTS.__FULL_PATH_TO_RESOURCES;
+            string basePath     = CONSTS.__FULL_PATH_TO_RESOURCES;
             string completePath = $"{basePath}{folderPath}\\{fileName}.cs";
             if (!Directory.Exists(basePath)) Directory.CreateDirectory(basePath);
             if (!File.Exists(completePath)) File.Create(completePath).Close();
             File.WriteAllText(completePath, $"public enum {fileName}\n{{\n\t {string.Join(",\n\t", items)} \n}}");
 
             MonoBehaviour.print($"Enum has been created in {completePath}");
+        }
+
+        public static bool Has(string folderName, string fileName)
+        {
+            return File.Exists($"{CONSTS.__FULL_PATH_TO_RESOURCES}{folderName}\\{fileName}.cs");
         }
     }
 }
