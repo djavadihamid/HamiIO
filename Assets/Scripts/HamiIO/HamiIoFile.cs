98e62a9fe0be91d2d8ea127b22c5d0cf1d15f87c
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-08-19 10:43:41 -0700

    [BUGFIX][IO_FILE] if directory not exist, it wont try to delete stuff

--- Assets/Scripts/HamiIO/HamiIoFile.cs
+++ Assets/Scripts/HamiIO/HamiIoFile.cs
@@ -6,6 +6,7 @@ namespace HamiIO
     {
         public static void DeleteAllFiles(string folder)
         {
+            if(!Directory.Exists(CONSTS.__FULL_PATH_TO_RESOURCES + folder)) return;
             DirectoryInfo di = new DirectoryInfo(CONSTS.__FULL_PATH_TO_RESOURCES + folder);
             foreach (FileInfo file in di.GetFiles()) file.Delete();
         }
