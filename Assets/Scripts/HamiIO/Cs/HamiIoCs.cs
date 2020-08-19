226d298e09f3c5f68d3f79894b7e34bd33bec230
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-08-18 23:33:56 -0700

    [FEAT][CS] make model

---
+++ Assets/Scripts/HamiIO/Cs/HamiIoCs.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using HamiIO.Cs;
+using UnityEngine;
+
+namespace HamiIO
+{
+    public class HamiIoCs
+    {
+        public static void WriteModel(string folderPath,
+            string fileName,
+            PropertiesModel[] properties,
+            string nameSpace = null,
+            string parent = null
+        )
+        {
+            FileChecker.HasCs(folderPath, fileName, true);
+            string content = "";
+            AddAndBreak(ref content, new[]
+            {
+                $"namespace {nameSpace}{{",
+                $"public class {fileName}{{",
+            });
+
+            foreach (PropertiesModel modelPropertiese in properties)
+            {
+                AddAndBreak(ref content, new[]
+                {
+                    modelPropertiese.Attribute,
+                    $"public {modelPropertiese.Type} {modelPropertiese.Name};"
+                });
+            }
+
+            File.WriteAllText($@"{CONSTS.__FULL_PATH_TO_RESOURCES}Model\{folderPath}\{fileName}.cs", content);
+            MonoBehaviour.print($@"CS has been created in {CONSTS.__FULL_PATH_TO_RESOURCES}{folderPath}\{fileName}");
+        }
+
+        private static void AddAndBreak(ref string reference, string[] toAdd)
+        {
+            foreach (string s in toAdd)
+            {
+                if (string.IsNullOrEmpty(s)) return;
+                reference += s + "\n";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HamiIO/Cs/PropertiesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HamiIO.Cs
+{
+    public class PropertiesModel
+    {
+        public string Attribute;
+        public string Type;
+        public string Name;
+    }
+}
