3cbae5fd7bcb2298c655ac1badb0109c30b64ff5
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-08-19 04:53:34 -0700

    [FEAT][CS] generating class with field and methods

--- Assets/Scripts/HamiIO/Cs/HamiIoCs.cs
+++ Assets/Scripts/HamiIO/Cs/HamiIoCs.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using HamiIO.Cs;
+using UnityEditor;
 using UnityEngine;
 
 namespace HamiIO
@@ -13,24 +14,122 @@ namespace HamiIO
         )
         {
             FileChecker.HasCs(folderPath + @"\Model", fileName, true);
-            string content = "";
-            if (!string.IsNullOrEmpty(nameSpace)) content += $"namespace {nameSpace}{{ \n";
-            content += $"public class {fileName}{{\n\n";
-            foreach (PropertiesModel modelPropertiese in properties)
-            {
-                content += string.IsNullOrEmpty(modelPropertiese.Attribute)
-                    ? null
-                    : $"\t[{modelPropertiese.Attribute}]\n";
-                content += string.IsNullOrEmpty(modelPropertiese.Type) || string.IsNullOrEmpty(modelPropertiese.Name)
-                    ? null
-                    : $"\tpublic {modelPropertiese.Type} {modelPropertiese.Name};\n\n";
-            }
-
-            content += string.IsNullOrEmpty(nameSpace) ? "}" : "}\n}";
-            FileChecker.HasCs(folderPath + @"\Model", fileName, true);
-            File.WriteAllText($@"{CONSTS.__FULL_PATH_TO_RESOURCES}{folderPath}\Model\{fileName}.cs", content);
+            File.WriteAllText($@"{CONSTS.__FULL_PATH_TO_RESOURCES}{folderPath}\Model\{fileName}.cs",
+                GenerateBody(fileName, nameSpace, properties, null));
             MonoBehaviour.print(
                 $@"CS has been created in {CONSTS.__FULL_PATH_TO_RESOURCES}{folderPath}\Model\{fileName}");
+        }
+
+
+        public static void GenerateClass(
+            string folderPath,
+            string fileName,
+            string parent = null,
+            string nameSpace = null,
+            PropertiesModel[] properties = null,
+            MethodsModel[] method = null
+        )
+
+        {
+            FileChecker.HasCs(folderPath, fileName, true);
+            File.WriteAllText($@"{CONSTS.__FULL_PATH_TO_RESOURCES}{folderPath}\{fileName}.cs",
+                GenerateBody(fileName, nameSpace, properties, method, parent));
+            MonoBehaviour.print(
+                $@"CS has been created in {CONSTS.__FULL_PATH_TO_RESOURCES}{folderPath}\{fileName}");
+        }
+
+        [MenuItem("hemiammid/asdasd")]
+        public static void Generate()
+        {
+            GenerateClass("folder", "HamiUrl", "Daddclass", "thi.name.s.a",
+                new[]
+                {
+                    new PropertiesModel
+                    {
+                        Name = "thisProperry",
+                        Type = "int"
+                    }
+                },
+                new[]
+                {
+                    new MethodsModel
+                    {
+                        Name = "Up",
+                        Arguments = null,
+                        AccessModifier = AccessModifiers.Public,
+                        ReturnType = "void",
+                        IsOverride = false
+                    }
+                }
+            );
+        }
+
+        private static string GenerateBody(
+            string fileName,
+            string nameSpace,
+            PropertiesModel[] properties,
+            MethodsModel[] methods,
+            string parent = ""
+        )
+        {
+            string content = string.Empty;
+            if (!string.IsNullOrEmpty(nameSpace)) content += $"namespace {nameSpace}{{ \n";
+
+            if (string.IsNullOrEmpty(parent))
+                AddTxt(ref content, $"public class {fileName}{{", 2);
+            else AddTxt(ref content, $"public class {fileName} : {parent}{{", 2);
+
+            if (properties != null)
+                foreach (PropertiesModel modelPropertiese in properties)
+                {
+                    AddTxt(ref content, modelPropertiese.Attribute, 1, 1, true);
+                    AddTxt(ref content, "public", 0, 1);
+                    AddTxt(ref content, modelPropertiese.Type);
+                    AddTxt(ref content, modelPropertiese.Name);
+                    AddTxt(ref content, ";", 2);
+                }
+
+            if (methods != null)
+                foreach (MethodsModel method in methods)
+                {
+                    AddTxt(ref content, method.AccessModifier.ToString().ToLower(), 0, 1);
+                    if (method.IsStatic) AddTxt(ref content, "static");
+                    else
+                    {
+                        if (method.IsVirtual) AddTxt(ref content, "virtual");
+                        else if (method.IsOverride) AddTxt(ref content, "override");
+                    }
+
+                    AddTxt(ref content, method.ReturnType);
+                    AddTxt(ref content, method.Name);
+                    AddTxt(ref content, "(");
+                    if (method.Arguments != null)
+                        foreach (ArgumentModel methodArgument in method.Arguments)
+                        {
+                            if (methodArgument.IsRefs) AddTxt(ref content, "refs");
+                            if (methodArgument.IsOut) AddTxt(ref content, "out");
+                            if (methodArgument.IsParams) AddTxt(ref content, "params");
+                            AddTxt(ref content, methodArgument.Type);
+                            AddTxt(ref content, methodArgument.Name);
+                        }
+
+                    AddTxt(ref content, ")", 1);
+                    AddTxt(ref content, "{", 1, 1);
+                    AddTxt(ref content, "throw new System.NotImplementedException();", 1, 2);
+                    AddTxt(ref content, "}", 1, 1);
+                }
+
+            content += string.IsNullOrEmpty(nameSpace) ? "}" : "}\n}";
+            return content;
+        }
+
+        private static void AddTxt(ref string content, string toAdd, byte breakAfter = 0, byte tabIndentBefore = 0,
+            bool isAttribute = false)
+        {
+            if (string.IsNullOrEmpty(toAdd)) return;
+            for (int i = 0; i < tabIndentBefore; i++) content += "\t";
+            content += isAttribute ? $"[{toAdd}]" : toAdd + " ";
+            for (int i = 0; i < breakAfter; i++) content += "\n";
         }
     }
 }
---
+++ Assets/Scripts/HamiIO/Cs/MethodsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HamiIO.Cs
+{
+    public class MethodsModel
+    {
+        public AccessModifiers AccessModifier;
+        public string Name;
+        public string ReturnType;
+        public ArgumentModel[] Arguments;
+        public bool IsOverride;
+        public bool IsVirtual;
+        public bool IsStatic;
+    }
+}
