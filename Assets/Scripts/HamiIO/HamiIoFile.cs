9026f556306e6f5d5e744aad033edf27b1d77aa6
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-08-19 06:20:26 -0700

    [FEAT][FILE] remove all files in a directory by a method

---
+++ Assets/Scripts/HamiIO/HamiIoFile.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace HamiIO
+{
+    public class HamiIoFile
+    {
+        public static void DeleteAllFiles(string folder)
+        {
+            DirectoryInfo di = new DirectoryInfo(CONSTS.__FULL_PATH_TO_RESOURCES + folder);
+            foreach (FileInfo file in di.GetFiles()) file.Delete();
+        }
+    }
+}
