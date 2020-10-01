48de3d231db62e500e37eafcfbfb9e44267bdcc1
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-10-01 18:28:32 +0330

    [REFACTOR][SCRIPTABLE_OBJECT] reading scriptable object from resource

--- Assets/Scripts/HamiIO/HamiScriptableObject.cs
+++ Assets/Scripts/HamiIO/HamiScriptableObject.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 namespace HamiIO
 {
-    public class HamiScriptableObject : Editor
+    public class HamiScriptableObject
     {
+#if UNITY_EDITOR
+        
         public static void Create<T>(string relativePath, string fileName) where T : ScriptableObject
         {
             if (!Directory.Exists($"Assets/Resources/Modules/{relativePath}"))
                 Directory.CreateDirectory($"Assets/Resources/Modules/{relativePath}");
-            T asset = CreateInstance<T>();
+            T asset = ScriptableObject.CreateInstance<T>();
             AssetDatabase.CreateAsset(asset, $"Assets/Resources/Modules/{relativePath}/{fileName}.asset");
             AssetDatabase.SaveAssets();
             EditorUtility.FocusProjectWindow();
             Selection.activeObject = asset;
+        }
+#endif
+
+        public static T LoadRuntime<T>(string relativePath, string fileName) where T : ScriptableObject
+        {
+            return Resources.Load<T>($"Modules/{relativePath}/{fileName}");
         }
     }
 }
