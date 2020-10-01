e336b0ee217c853db30869bd20feca5e224258b6
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-10-01 18:09:19 +0330

    [FEAT][SCRIPTABLE_OBJECT] check for folder existance

--- Assets/Scripts/HamiIO/HamiScriptableObject.cs
+++ Assets/Scripts/HamiIO/HamiScriptableObject.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace HamiIO
@@ -7,6 +8,8 @@ namespace HamiIO
     {
         public static void Create<T>(string relativePath, string fileName) where T : ScriptableObject
         {
+            if (!Directory.Exists($"Assets/Resources/{relativePath}"))
+                Directory.CreateDirectory($"Assets/Resources/{relativePath}");
             T asset = CreateInstance<T>();
             AssetDatabase.CreateAsset(asset, $"Assets/Resources/{relativePath}/{fileName}.asset");
             AssetDatabase.SaveAssets();
