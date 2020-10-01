befb3ef75c79f442205e69c50be0408795b108ef
Hamid Djavadi <djavadi.hamid@yandex.com>
2020-10-01 17:59:59 +0330

    [FEAT][SCRIPTABLE_OBJECT] add new scriptable object

---
+++ Assets/Scripts/HamiIO/HamiScriptableObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace HamiIO
+{
+    public class HamiScriptableObject : Editor
+    {
+        public static void Create<T>(string relativePath, string fileName) where T : ScriptableObject
+        {
+            T asset = CreateInstance<T>();
+            AssetDatabase.CreateAsset(asset, $"Assets/Resources/{relativePath}/{fileName}.asset");
+            AssetDatabase.SaveAssets();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = asset;
+        }
+    }
+}
